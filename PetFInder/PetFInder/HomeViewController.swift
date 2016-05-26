5b32d11535ee90f3520f1673d6a09bdbab1fe570
matiasdim <matiasdim3@gmail.com>
2016-05-25 22:11:57 -0400

    Navigation controller added, also home VC added

---
+++ PetFInder/PetFInder/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  PetFInder
+//
+//  Created by Matías  Gil Echavarría on 5/25/16.
+//  Copyright © 2016 Matías Gil Echavarría. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
