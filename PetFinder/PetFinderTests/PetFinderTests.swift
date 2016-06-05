eb9693ea169f7a7d92c29c79d7a21885199a18c6
matiasdim <matiasdim3@gmail.com>
2016-06-04 23:39:54 -0400

    UTests for network manager

--- PetFinder/PetFinderTests/PetFinderTests.swift
+++ PetFinder/PetFinderTests/PetFinderTests.swift
@@ -12,7 +12,10 @@ import XCTest
 
 class PetFinderTests: XCTestCase {
     
-    func testMealInitialization() {
+    // MARK: - Model tests
+    
+    //MARK: User model test
+    func testUserInitialization() {
         // Success case.
         let user = User(email: "email@email.co", password: "123123123")
         XCTAssertNotNil(user)
@@ -25,47 +28,50 @@ class PetFinderTests: XCTestCase {
         XCTAssertNil(noPasswordUser, "Empty password is invalid")
         
     }
+    
+    // MARK: - Network tests
+    
+    //MARK: User calls
+    func testNetworkManagerCreateUser()
+    {
+        let asyncExpectation = expectationWithDescription("asynchronous request")
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let nm = NetworkManager()
+        nm.createUser(["email": "m@m.test","password": "123"], successCallback:
+            { (response) in
+                XCTAssertNotNil(response, "WebServices succesfully called. User created")
+                asyncExpectation.fulfill()
+            }) { (error) in
+                XCTAssertNotNil(error, "WebServices succesfully called. User not created")
+                asyncExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(5) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.c
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testNetworkManagerLoginUser()
+    {
+        let asyncExpectation = expectationWithDescription("asynchronous request")
+        
+        let nm = NetworkManager()
+        nm.loginUser(["email": "m@m.test","password": "123"], successCallback:
+            { (response) in
+                XCTAssertNotNil(response, "WebServices succesfully called. User created")
+                asyncExpectation.fulfill()
+        }) { (error) in
+            XCTAssertNotNil(error, "WebServices succesfully called. User not created")
+            asyncExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(5) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
         }
     }
     
 }
-
-/*
- func testMealInitialization() {
- // Success case.
- let user = User(email: "email@email.co", password: "123123123")
- XCTAssertNotNil(user)
- 
- // Failure cases.
- let noEmailUser = User(email: "", password: "234234234")
- XCTAssertNil(noEmailUser, "Empty email is invalid")
- 
- let noPasswordUser = User(email: "m@m.co", password: "")
- XCTAssertNil(noPasswordUser, "Empty password is invalid")
- 
- }
- */
-
-
-
-
